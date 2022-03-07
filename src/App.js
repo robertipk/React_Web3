5defe11f04bd3fd21951e69559fef5f22adaf455
Rob Ip <robertipk@gmail.com>
2022-03-07 10:31:44 -0800

    react work deployed and working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from './config';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [account, setAccount] = useState(); // state variable to set account.
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+      // Instantiate smart contract using ABI and address.
+      const taskContract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+      // Get total number of tasks
+      const counter = await taskContract.methods.count().call();
+      for (var i = 1; i <= counter; i++) {
+        // Retrieve every task from the tasks variable on smart contract
+        const task = await taskContract.methods.tasks(i).call();
+        // Add the the task to our list of tasks.
+        setTasks((tasks) => [...tasks, task]);
+      }
+    }
+   load();
+  }, []);
+  
+   return (
+     <div>
+       Account address: {account}
+       <h1>Tasks</h1>
+      <ol>
+      {
+        Object.keys(tasks).map((_, index) => (
+          <li>
+            <h4>Task: {tasks[index].name}</h4>
+            <span><b>Description: </b>{tasks[index].description}</span>
+          </li>
+        ))
+      }
+      </ol>
+     </div>
+   );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,70 @@
+export const CONTRACT_ADDRESS = '0x70685118b739b8e8D71DE69ABA83515422b6EB61';
+export const CONTRACT_ABI = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [],
+        "name": "count",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "tasks",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "internalType": "string",
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "description",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_name",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_task",
+            "type": "string"
+          }
+        ],
+        "name": "createTask",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+];
